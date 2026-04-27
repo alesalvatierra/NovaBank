ff4cbc2a975d081fb7de09604d285eceddb027b8
Alejandro Lopez Salvatierra Ruiz <alopsalv@emeal.nttdata.com>
2026-04-27 10:39:48 +0200

    build: configuracion inicial de springboot y application.yml

---
+++ src/main/java/org/alopsalv/novabank/NovaBankApplication.java
@@ -0,0 +1,12 @@
+package org.alopsalv.novabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaBankApplication.class, args);
+    }
+}
